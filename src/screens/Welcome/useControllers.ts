adb6f18031566e2b2db42459f273efd3fa3056fd
HamedDiaby <hameddiaby.koumba@gmail.com>
2025-09-10 19:11:24 +0200

    Refactor Welcome screen to simplify layout and remove unused secondary action; update useControllers for cleaner navigation handling

--- src/screens/Welcome/useControllers.ts
+++ src/screens/Welcome/useControllers.ts
@@ -1,23 +1,20 @@
 
+import { NavigationStackEnum } from "@utils/enums";
+import { useNavigation } from "@utils/hooks";
 import { useTranslation } from "@utils/translations";
+import { useCallback } from "react";
 
 export const useControllers = () => {
 
     const { t } = useTranslation();
+    const { onNavigateTo } = useNavigation();
 
-    const handlePrimaryAction = () => {
-        // TODO: Implementer l'action principale (ex: navigation vers la page d'inscription)
-        console.log('Primary action pressed');
-    };
-
-    const handleSecondaryAction = () => {
-        // TODO: Implementer l'action secondaire (ex: navigation vers plus d'informations)
-        console.log('Secondary action pressed');
-    };
+    const handlePrimaryAction = useCallback(() => {
+        onNavigateTo({ stack: NavigationStackEnum.MAIN_TABS });
+    }, [onNavigateTo]);
 
     return {
         t,
         handlePrimaryAction,
-        handleSecondaryAction,
     }
 };
